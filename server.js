8384aad8ee0dfdd1eb84148d5e9ce24aa4612f81
Sasank Yadavalli <yadavallisasank@gmail.com>
2020-06-02 08:21:08 +0530

    Add db and auth config files

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
+
+// Connect database
+connectDB();
+
+//Init middleware
+app.use(express.json({extended: true}));
+
 
 app.get('/', (req, res) => 
     res.json({msg: 'Welcome to contactkeeper API'})
